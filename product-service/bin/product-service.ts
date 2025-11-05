5bde8994481d3c0c74ba889be0a9d41cadc7f3c3
Viktoryia Liaukovich <Viktoryia_Liaukovich@epam.com>
2025-11-05 19:40:31 +0100

    feat: add task 8

--- product-service/bin/product-service.ts
+++ product-service/bin/product-service.ts
@@ -4,6 +4,7 @@ import { ProductServiceStack } from '../lib/product-service-stack';
 import { ImportServiceStack } from '../lib/import-service-stack';
 import { AuthorizationServiceStack } from '../lib/authorization-service-stask';
 import { NestServiceStack } from '../lib/nest-service-stack';
+import { CartRdsStack } from '../lib/cart-rds-stack';
 
 const app = new cdk.App();
 const productServiceStack = new ProductServiceStack(app, 'ProductServiceStack', {});
@@ -14,4 +15,12 @@ new ImportServiceStack(app, 'ImportServiceStack', {
   catalogItemsQueue: productServiceStack.catalogItemsQueue,
 });
 
-new NestServiceStack(app, 'NestServiceStack', {});
+// Create RDS stack first
+const cartRdsStack = new CartRdsStack(app, 'CartRdsStack', {});
+
+// Create Nest Service stack with RDS dependencies
+new NestServiceStack(app, 'NestServiceStack', {
+  database: cartRdsStack.database,
+  vpc: cartRdsStack.vpc,
+  dbSecurityGroup: cartRdsStack.dbSecurityGroup,
+});
